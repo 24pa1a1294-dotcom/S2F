1e55399fc4243c7ea8f189d99f6bf3641079a6e8
Akhila Kola <akhila@gmail.com>
2026-03-05 06:41:40 +0530

    Configure Firebase for skill-to-force

---
+++ frontend/src/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAzfodbPalwFuL2bLiCbRUQGC4rnb6DGDc",
+    authDomain: "skill-to-force.firebaseapp.com",
+    projectId: "skill-to-force",
+    storageBucket: "skill-to-force.firebasestorage.app",
+    messagingSenderId: "499077067376",
+    appId: "1:499077067376:web:2d2f165245ff2987b23ab5",
+    measurementId: "G-FZG6E1Z4B7"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
